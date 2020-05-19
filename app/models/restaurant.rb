4a48f6fd36be6a026159ab6328bebe2a8b64a63f
ToveXan <zxanoswork@gmail.com>
2020-05-19 10:55:52 -0500

    commit four, more editting to def

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,6 +7,12 @@ class Restaurant
 
   def reviews
     #returns an array of all reviews for that restaurant
+    #made an array, but returned [false,false,false,]
+    #working on how to change it but time ran out
+    all_reviews = Review.all.map do |each_review|
+      each_review.restaurant == @name
+    end
+    p all_reviews
   end
   def average_star_rating
     #returns the average star rating for a restaurant based on its reviews
