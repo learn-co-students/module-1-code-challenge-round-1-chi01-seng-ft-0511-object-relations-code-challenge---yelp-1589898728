f9b337282166e6eba8eac3f794d722bd513c2218
johnloberger <johnloberger@gmail.com>
2020-05-19 10:02:42 -0500

    initial shameless green

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,45 @@
 class Customer
   attr_accessor :given_name, :family_name
 
+  @@all = []
+
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def reviews
+    Review.all.select {|review| review.customer == self}
+  end
+
+  def restaurants
+    self.reviews.map &:restaurant
+  end
+
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant, rating)
+  end
+
+  def num_reviews
+    self.reviews.count
+  end
+
+  def self.find_by_name(full_name)
+    self.all.select {|customer| customer.full_name == full_name}
+  end
+
+  def self.find_all_by_given_name(given_name)
+    self.all.select {|customer| customer.given_name == given_name}
+  end
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,27 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    self.reviews.map &:customer
+  end
+
+  def average_star_rating
+    (self.reviews.sum &:rating) / self.reviews.count.to_f
   end
 
 end
