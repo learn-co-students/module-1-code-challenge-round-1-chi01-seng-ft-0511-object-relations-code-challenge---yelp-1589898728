31aabdceeaa935a1335c1bdd5e3c81bad66627bc
emilioquintana90 <emilioquintana90@gmail.com>
2020-05-19 10:08:36 -0500

    Finished coding challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,16 @@ class Restaurant
     @name = name
   end
 
+  def reviews
+    Review.all.select {|r| r.restaurant  == self}
+  end
+
+  def customers
+    (reviews.map {|r| r.customer}).uniq
+  end
+
+  def average_star_rating
+    (reviews.map {|r| r.rating}).sum.to_f / reviews.count
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,19 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+# ******* CUSTOMER INSTANCES ********
+emilio = Customer.new("Emilio", "Quintana")
+john = Customer.new("John", "Smith")
+
+# ******* RESTAURANT INSTANCES ********
+purple_pig = Restaurant.new("Purple Pig")
+giordanos = Restaurant.new("Giordanos")
+
+# ******* REVIEW INSTANCES ********
+Review.new(emilio, purple_pig, 10)
+Review.new(john, purple_pig, 5)
+Review.new(emilio, giordanos, 8)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
