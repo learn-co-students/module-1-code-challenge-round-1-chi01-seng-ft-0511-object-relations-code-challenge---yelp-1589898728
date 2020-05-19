c690f214a6a9bd66d2cbadda604ac8d7e63bfbed
ToveXan <zxanoswork@gmail.com>
2020-05-19 10:17:15 -0500

    commit 1, testing initializers

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,9 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+john_h = Customer.new("John", "Hamish")
+binging_brunch = Restaurant.new("Binging Brunch")
+john_h_review = Review.new(john_h, binging_brunch, 4.5)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
